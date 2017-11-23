c2d6916ada1a31682353831c0e4fc7a4a62e69dd
Pawel Kowaliszyn <pkowaliszyn@onet.eu>
2017-11-23 14:12:28 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// alert('Witaj,' + name);
+console.log('Witaj,' + name);
+
+//  to test
+
+/*
+var name = prompt('Enter your name');
+alert('Witaj,' + name);
+console.log('Witaj,' + name);
+*/
